aded2e3b8d29874e829734c8a8cf7c774bb10f84
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-09-29 22:25:58 +0900

    Modify routes.rb, application.scss, and add custom.scss in src

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/show'
-  get 'stores/new'
-  get 'stores/edit'
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'reservations/new'
-  get 'reservations/edit'
   root 'static_pages#top'
-  get 'users/index'
 
+  resources :reservations
+  resources :stores
+  
+  get 'users/index'
   devise_for :users
 end
