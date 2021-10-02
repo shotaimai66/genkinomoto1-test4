a374dc74feb893c2a4b83d1fc7c74750c2b5dc94
tryoma <tryoma0326@gmail.com>
2021-10-02 19:21:24 +0900

    staff_devise追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      root_path
+    when Staff
+      staffs_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
   resources :reservations
   resources :stores
-  
+
+  get 'users/index'
   devise_for :users
+  devise_for :staffs, controllers: {
+    sessions:      'staffs/sessions',
+    passwords:     'staffs/passwords',
+    registrations: 'staffs/registrations'
+  }
+  resources :staffs, only: %i[index]
 end
